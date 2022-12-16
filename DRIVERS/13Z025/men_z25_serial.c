7fbfe1ac127a778264676c3f549f2b1d2cd7024b
Javier Rodriguez <josejavier.rodriguez@duagon.com>
2022-12-16 14:29:44 +0100

    Fix compilation error for kernel versions greater than V5.14
    In V5.14 the support for legacy IDE was removed.
    
    Commit: b7fb14d3ac63117e0e8beabe75f4ea52051fbe3a
    
    Z025 is not actually using anything related with IDE peripehals
    as it is a serial driver.

--- DRIVERS/13Z025/men_z25_serial.c
+++ DRIVERS/13Z025/men_z25_serial.c
@@ -30,7 +30,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/ioport.h>
-#include <linux/ide.h>
 #include <linux/init.h>
 #include <linux/serial.h>
 #include <linux/version.h>
